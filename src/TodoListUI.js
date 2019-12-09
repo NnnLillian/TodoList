0cbae7671513002a2bbfc0f424b2f8488bbd672e
Lillian <weijingwen35@foxmail.com>
2019-12-09 18:44:14 +0800

    将UI组件转换为无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,32 +1,30 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import 'antd/dist/antd.css';
 import { Input, Button, List, Typography, Tag } from 'antd';
 
-class TodoListUI extends Component {
-    render() {
-        return (
-            <Fragment >
-                <div style={{ marginTop: "16px", marginLeft: '16px' }}>
-                    <Input placeholder="新增待办事项"
-                        style={{ width: '300px', marginRight: '16px' }}
-                        value={this.props.inputValue}
-                        onChange={this.props.handleInputChange}
-                    />
-                    <Button type="primary" onClick={this.props.handleButtonClick}> 提交 </Button>
-                </div>
-                <List style={{ marginTop: '16px', marginLeft: '16px', width: '300px' }}
-                    bordered
-                    dataSource={this.props.list}
-                    renderItem={(item, index) => (
-                        <List.Item onClick={(index)=>{this.props.handleItemDelete(index)}}>
-                            <Typography.Text mark > 待办: </Typography.Text>{item}
-                            <Tag style={{ marginLeft: '8px' }} > 单击删除 </Tag>
-                        </List.Item>
-                    )}
+const TodoListUI = (props) => {
+    return (
+        <Fragment >
+            <div style={{ marginTop: "16px", marginLeft: '16px' }}>
+                <Input placeholder="新增待办事项"
+                    style={{ width: '300px', marginRight: '16px' }}
+                    value={props.inputValue}
+                    onChange={props.handleInputChange}
                 />
-            </Fragment>
-        )
-    }
+                <Button type="primary" onClick={props.handleButtonClick}> 提交 </Button>
+            </div>
+            <List style={{ marginTop: '16px', marginLeft: '16px', width: '300px' }}
+                bordered
+                dataSource={props.list}
+                renderItem={(item, index) => (
+                    <List.Item onClick={(index) => { props.handleItemDelete(index) }}>
+                        <Typography.Text mark > 待办: </Typography.Text>{item}
+                        <Tag style={{ marginLeft: '8px' }} > 单击删除 </Tag>
+                    </List.Item>
+                )}
+            />
+        </Fragment>
+    )
 }
 
 export default TodoListUI;
