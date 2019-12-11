590f88ea260bc80a85f47a3c7b4ca3ad893ef4eb
Lillian <weijingwen35@foxmail.com>
2019-12-11 17:03:54 +0800

    在saga基础是增加了react-redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react'
 
 import TodoListUI from './TodoListUI'
-
+import { connect } from 'react-redux';
 import store from './store/index'
 import { getInputChangeAction, getAddAction, deleteItem, getInitListAction } from './store/actionCreators'
 
 class TodoList extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = store.getState();
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleButtonClick = this.handleButtonClick.bind(this);
-        this.handStoreChange = this.handStoreChange.bind(this);
-        this.handleItemDelete = this.handleItemDelete.bind(this);
-        store.subscribe(this.handStoreChange);
-    }
-
     render() {
+
+        const { inputValue, list, handleButtonClick, handleInputChange, handleItemDelete } = this.props
+
         return (
             <TodoListUI
-                inputValue={this.state.inputValue}
-                list={this.state.list}
-                handleInputChange={this.handleInputChange}
-                handleButtonClick={this.handleButtonClick}
-                handleItemDelete={this.handleItemDelete}
+                inputValue={inputValue}
+                list={list}
+                handleInputChange={handleInputChange}
+                handleButtonClick={handleButtonClick}
+                handleItemDelete={handleItemDelete}
             />
         )
     }
@@ -33,24 +26,32 @@ class TodoList extends Component {
         const action = getInitListAction();
         store.dispatch(action);
     }
+}
 
-    handleInputChange(e) {
-        const action = getInputChangeAction(e.target.value);
-        store.dispatch(action);
-    }
-
-    handleButtonClick() {
-        const action = getAddAction();
-        store.dispatch(action);
-    }
-
-    handleItemDelete(index) {
-        const action = deleteItem(index);
-        store.dispatch(action);
-    }
-
-    handStoreChange() {
-        this.setState(store.getState());
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
     }
 }
-export default TodoList;
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        handleInputChange(e) {
+            const action = getInputChangeAction(e.target.value);
+            dispatch(action);
+        },
+
+        handleButtonClick() {
+            const action = getAddAction();
+            dispatch(action);
+        },
+
+        handleItemDelete(index) {
+            const action = deleteItem(index);
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoList from './TodoList'
+import store from './store/index'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+const APP = (
+    <Provider store={store}>
+        <TodoList />
+    </Provider>
+)
+
+ReactDOM.render(APP, document.getElementById('root'));
